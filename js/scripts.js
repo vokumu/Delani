29267129fe1052b0f4ca835723d70b0a6420e073
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-06-18 20:11:09 +0300

    Added hover functionality and name to protfolio

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,109 @@ $(document).ready(function() {
         $(".devImg").toggle();
         $(".devContent").toggle();
       });
-      $(".product").click(function() {
+    $(".product").click(function() {
         $(".prodImg").toggle();
         $(".prodContent").toggle();
+    });
+    
+   /* for (var i = 0; i < 8; i++) {
+      //console.log('.text'+i);
+        $('#wb_Image'+[i]+' '+' a img').hover(function () {
+          $(this).stop().animate({
+              opacity: .3
+          }, 200);
+          for (var i = 0; i < 8; i++) {
+          $('.text'+i).removeClass('hide')
+          }
+          
+      }, function () {
+          $(this).stop().animate({
+              opacity: 1}, 600);
+          for (var i = 0; i < 8; i++) {
+          $('.text'+i).addClass('hide');
+          }
+        }
       });
+    
+    }*/
+    $('#wb_Image0 a img').hover(function () {
+      $(this).stop().animate({
+          opacity: .3
+      }, 200);
+      $('.text0').removeClass('hide');
+  }, function () {
+      $(this).stop().animate({
+          opacity: 1}, 600);
+      $('.text0').addClass('hide');
   });
+      $('#wb_Image1 a img').hover(function () {
+      $(this).stop().animate({
+          opacity: .3
+      }, 200);
+      $('.text1').removeClass('hide');
+  }, function () {
+      $(this).stop().animate({
+          opacity: 1}, 600);
+      $('.text1').addClass('hide');
+  });
+  $('#wb_Image2 a img').hover(function () {
+    $(this).stop().animate({
+        opacity: .3
+    }, 200);
+    $('.text2').removeClass('hide');
+}, function () {
+    $(this).stop().animate({
+        opacity: 1}, 600);
+    $('.text2').addClass('hide');
+});
+$('#wb_Image3 a img').hover(function () {
+  $(this).stop().animate({
+      opacity: .3
+  }, 200);
+  $('.text3').removeClass('hide');
+}, function () {
+  $(this).stop().animate({
+      opacity: 1}, 600);
+  $('.text3').addClass('hide');
+});
+$('#wb_Image4 a img').hover(function () {
+  $(this).stop().animate({
+      opacity: .3
+  }, 200);
+  $('.text4').removeClass('hide');
+}, function () {
+  $(this).stop().animate({
+      opacity: 1}, 600);
+  $('.text4').addClass('hide');
+});
+$('#wb_Image5 a img').hover(function () {
+  $(this).stop().animate({
+      opacity: .3
+  }, 200);
+  $('.text5').removeClass('hide');
+}, function () {
+  $(this).stop().animate({
+      opacity: 1}, 600);
+  $('.text5').addClass('hide');
+});
+$('#wb_Image6 a img').hover(function () {
+  $(this).stop().animate({
+      opacity: .3
+  }, 200);
+  $('.text6').removeClass('hide');
+}, function () {
+  $(this).stop().animate({
+      opacity: 1}, 600);
+  $('.text6').addClass('hide');
+});
+$('#wb_Image7 a img').hover(function () {
+  $(this).stop().animate({
+      opacity: .3
+  }, 200);
+  $('.text7').removeClass('hide');
+}, function () {
+  $(this).stop().animate({
+      opacity: 1}, 600);
+  $('.text7').addClass('hide');
+});
+});
